8e8f875d1cfe5992034f60799e4a5809014d78f5
lonelu <lonelur@gmail.com>
2021-11-18 11:55:42 -0800

    fix CYS

--- scrips/search_selfcenter/run_selfcenter_search.py
+++ scrips/search_selfcenter/run_selfcenter_search.py
@@ -28,14 +28,14 @@ print(len(all_querys))
 
 ### run Search_struct
 
-workdir = '/mnt/e/DesignData/ligands/LigandBB/MID1sc10/'
+# workdir = '/mnt/e/DesignData/ligands/LigandBB/MID1sc10/'
 
-outdir = workdir + 'output_selfcenter/'
+# outdir = workdir + 'output_selfcenter/'
 
-target_path = workdir + '5od1_zn.pdb'
+# target_path = workdir + '5od1_zn.pdb'
 
-#win_filter = []
-win_filter = [35,  61,  65]
+# #win_filter = []
+# win_filter = [35,  61,  65]
 
 
 # workdir = '/mnt/e/DesignData/ligands/LigandBB/6dwv/'
@@ -46,6 +46,14 @@ win_filter = [35,  61,  65]
 
 # win_filter = []
 
+workdir = '/mnt/e/DesignData/ligands/LigandBB/8adh/'
+
+outdir = workdir + 'output_selfcenter/'
+
+target_path = workdir + '1989_8adh_ZN_1.pdb'
+
+win_filter = []
+
 geometry_path = None
 #geometry_path = workdir + 'tetrahydral_geo.pdb'
 
@@ -53,11 +61,12 @@ metal_metal_dist = 0.45
 
 num_contact_vdms = [3]
 
-allowed_aa_combinations = [['H', 'H', 'H']]
+#allowed_aa_combinations = [['H', 'H', 'H']]
+allowed_aa_combinations = []
 
 _filter = filter.Search_filter(filter_abple = False, filter_phipsi = True, max_phipsi_val = 25, 
     filter_vdm_score = False, min_vdm_score = 0, filter_vdm_count = False, min_vdm_clu_num = 20,
-    after_search_filter_geometry = True, filter_based_geometry_structure = False, angle_tol = 15, aa_aa_tol = 0.3, aa_metal_tol = 0.2,
+    after_search_filter_geometry = False, filter_based_geometry_structure = False, angle_tol = 15, aa_aa_tol = 0.3, aa_metal_tol = 0.2,
     pair_angle_range = [92, 116], pair_aa_aa_dist_range = [3.0, 3.5], pair_metal_aa_dist_range = None,
     after_search_filter_qt_clash = True, vdm_vdm_clash_dist = 2.7, vdm_bb_clash_dist = 2.2, 
     write_filtered_result = False, selfcenter_filter_member_phipsi=True)

6a3f428c8412e983029c1fa6d596e8e3b63affbf
lonelu <lonelur@gmail.com>
2021-11-18 10:26:18 -0800

    fix ram with mask matrix

--- metalprot/search/find_path_by_matrix.py
+++ metalprot/search/find_path_by_matrix.py
@@ -4,6 +4,10 @@ import itertools
 from numpy.core.shape_base import _block_slicing
 import prody as pr
 import datetime
+from scipy.sparse.csr import csr_matrix
+from scipy.sparse import lil_matrix
+
+from scipy.sparse.sputils import matrix
 
 from ..basic import hull
 from ..basic import utils
@@ -58,7 +62,7 @@ paths = calc_adj_matrix_paths(m_adj_matrix)
 def calc_adj_matrix_paths(m_adj_matrix, num_iter =3):
     paths = []
     for r in range(m_adj_matrix.shape[0]):
-        inds = np.where(m_adj_matrix[r])[0]
+        inds = m_adj_matrix.rows[r]
         if len(inds) < num_iter-1:
             continue
         for _comb in itertools.combinations(inds, num_iter-1):
@@ -111,11 +115,13 @@ def neighbor_generate_nngraph(ss):
     nbrs_bb = NearestNeighbors(radius= 3.5).fit(all_coords)
     adj_matrix_bb = nbrs_bb.radius_neighbors_graph(bb_coords).astype(bool)
     #print(adj_matrix_bb.shape)
-    #create mask
-    mask = generate_filter_mask(ss, wins, win_labels, metal_vdm_size, adj_matrix_bb)
 
-    #calc modified adj matrix
-    m_adj_matrix = adj_matrix.multiply(mask)
+    # #create mask
+    # mask = generate_filter_mask(ss, wins, win_labels, metal_vdm_size, adj_matrix_bb)
+    # #calc modified adj matrix
+    # m_adj_matrix = adj_matrix.multiply(mask)
+
+    m_adj_matrix = filter_adj_matrix(ss, wins, metal_vdm_size, adj_matrix, adj_matrix_bb)
 
     return m_adj_matrix, win_labels, vdm_inds
 
@@ -197,3 +203,77 @@ def generate_filter_mask(ss, wins, win_labels, metal_vdm_size, adj_matrix_bb):
         win_mask *= labels_m  
             
     return win_mask
+
+
+def filter_adj_matrix(ss, wins, metal_vdm_size, adj_matrix, adj_matrix_bb):
+    '''
+    One issue for the mask method is that the matrix is sparse, there will be a lot of unnecessary calculation.
+    For example, filter phi psi, if a vdm is never be in any neighbor, there is no need to calc it.
+    '''
+        
+    wins = sorted(list(ss.neighbor_query_dict.keys()))
+    metal_vdm_size = len(ss.all_metal_vdm.get_metal_mem_coords())    
+    
+    mask_labels = np.ones(len(wins)*metal_vdm_size, dtype=bool)
+
+    # Metal bb Clashing filter
+    for i in range(adj_matrix_bb.shape[0]):
+        mask_labels *= ~(adj_matrix_bb[i].toarray().reshape(len(mask_labels),))
+
+
+    #aa origin filter. 
+    if ss.validateOriginStruct:
+        v_aa = np.array([v.aa_type for v in ss.vdms])
+        ress = [one_letter_code[ss.target.select('name CA and resindex ' + str(wx)).getResnames()[0]] for wx in wins]
+        aa_labels = np.zeros(len(wins)*metal_vdm_size, dtype=bool)
+        for inx in range(len(wins)):
+            aa_labels[inx*metal_vdm_size:(inx+1)*metal_vdm_size] = v_aa == ress[inx]
+        mask_labels *= aa_labels
+
+    # abple filter
+    if ss.search_filter.filter_abple:
+        v_abples = np.array([v.abple for v in ss.vdms])
+        apxs = [ss.target_abple[wx] for wx in wins]
+        abple_labels = np.zeros(len(wins)*metal_vdm_size, dtype=bool)
+        for inx in range(len(wins)):
+            abple_labels[inx*metal_vdm_size:(inx+1)*metal_vdm_size] = v_abples == apxs[inx]
+        mask_labels *= abple_labels
+
+    #Filter unwanted amino acids. if ss.allowed_aas = {'H', 'D'}, then {'E', 'S'} will be eliminated.
+    if not ss.validateOriginStruct and len(ss.allowed_aas) > 0 and len(ss.allowed_aas) < 4:
+        v_aa = np.array([v.aa_type for v in ss.vdms])
+        aa_allow_labels = np.zeros(len(wins)*metal_vdm_size, dtype=bool)
+        for inx in range(len(wins)):
+            aa_allow_labels[inx*metal_vdm_size:(inx+1)*metal_vdm_size] = np.array([v in ss.allowed_aas for v in v_aa])
+        mask_labels *= aa_allow_labels
+
+    #phi psi filter
+    if ss.search_filter.filter_phipsi:
+        #TO DO: filter phi psi need to be changed to be able to broadcast.
+        v_phis = [v.phi for v in ss.vdms]
+        v_psis = [v.psi for v in ss.vdms]
+
+        phis = [ss.phipsi[wx][0] for wx in wins]
+        psis = [ss.phipsi[wx][1] for wx in wins]
+        phipsi_labels = np.zeros(len(wins)*metal_vdm_size, dtype=bool)
+        for inx in range(len(wins)):  
+            for i in range(metal_vdm_size):
+                phi_ok = utils.filter_phipsi(phis[inx], v_phis[i], ss.search_filter.max_phipsi_val)
+                psi_ok = utils.filter_phipsi(psis[inx], v_psis[i], ss.search_filter.max_phipsi_val)
+                if phi_ok and psi_ok:
+                    phipsi_labels[inx*metal_vdm_size + i] = True
+        mask_labels *= phipsi_labels
+
+    m_adj_matrix = lil_matrix(adj_matrix.shape, dtype=bool)
+    for r in range(adj_matrix.shape[0]):
+        if not mask_labels[r]:
+            continue 
+
+        for ind in range(adj_matrix.indptr[r], adj_matrix.indptr[r+1]):
+            c = adj_matrix.indices[ind]
+            
+            # vdm on the same position don't connect.
+            if c > r and mask_labels[c] and r//metal_vdm_size != c//metal_vdm_size:
+                m_adj_matrix[r, c] = True
+            
+    return m_adj_matrix

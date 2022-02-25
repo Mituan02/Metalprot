a2243b24c7e8429708a1ed9b51373657abf93a51
lonelu <lonelur@gmail.com>
2022-02-24 16:44:57 -0800

    tts rdkit

--- scrips/position_ligand/ntf2_1dmm/run_search_ligand_indep_rdkit.py
+++ scrips/position_ligand/ntf2_1dmm/run_search_ligand_indep_rdkit.py
@@ -21,7 +21,7 @@ import shutil
 import datetime
 
 '''
-python /mnt/e/GitHub_Design/Metalprot/scrips/position_ligand/ntf2_1dmm_i3pa/run_search_ligand_indep_rdkit.py 0 4
+python /mnt/e/GitHub_Design/Metalprot/scrips/position_ligand/ntf2_1dmm/run_search_ligand_indep_rdkit.py  0 4
 '''
 
 def run_ligand(outdir, target, lig_path):
@@ -70,7 +70,7 @@ def run_search(target, ligs, path_to_database, ideal_ala_coords):
 
 
                     for cg_id, lig_id, _rmsd, vdm_ag, vdm_id, score, v, contact_hb, contact_cc in results:
-                        prefix = 'Lig-' + str(lig_id) + '_' + cg  + '_' + aa + '_' + str(resnum) + '_rmsd_' + str(round(_rmsd, 2)) + '_v_' + str(round(score, 1)) + '_'       
+                        prefix = 'Lig-' + str(lig_id) + '_' + cg + '_' + str(resnum) + '_' + aa + '_rmsd_' + str(round(_rmsd, 2)) + '_v_' + str(round(score, 1)) + '_'       
 
                         #pr.writePDB(outdir_all + prefix + str(vdm_id) + '.pdb.gz', vdm_ag)
                         #summaries.append((cg, aa, resnum, cg_id, lig_id, _rmsd, vdm_id, score))
@@ -94,14 +94,13 @@ def run_search(target, ligs, path_to_database, ideal_ala_coords):
 
 predefined_win_filters = [15, 19, 27]
 
-ro1 = ['C2', 'C3']
-rest1 = ['H1', 'C1', 'O1', 'O2', 'O3', 'FE1']
-ro2 = ['C3', 'C4']
-rest2 = ['H2', 'H3', 'H1', 'C2', 'C1', 'O1', 'O2', 'O3', 'FE1']
+ro1 = ['C8', 'C7']
+rest1 = ['H7', 'C9', 'O1', 'O3', 'O2', 'FE1']
+ro2 = ['C7', 'C6']
+rest2 = ['H5', 'H6', 'H7', 'C8', 'C9', 'O1', 'O3', 'O2', 'FE1']
 
-#lig_connects = [['FE1', 'O3','O1'], ['FE1', 'O1','O3']]
-lig_connects = [['FE1', 'O2','O3']]  #On ligand
-geo_sel = 'chid X and name FE1 O2 O3' #On protein
+lig_connects = [['FE1', 'O3','O1']]
+geo_sel = 'chid X and name FE1 O2 O3'
 clash_dist = 3.0
 
 ########################################################################
@@ -110,186 +109,156 @@ clash_dist = 3.0
 load_cg_aa_vdm_dict = {
     #'coo': [('AGKNQRSTY', True, False)], # (aas, filter_hb, filter_cc)
     #'bb_cco': [('AGKNQRSTY', True, False)],
-    'indole': [('AGDEKNQRST', True, False), ('FWY', False, True)],
-    'hid': [('AGDEKNQRST', True, False), ('FWY', False, True)],
-    'hie': [('AGDEKNQRST', True, False), ('FWY', False, True)],
+    'phenol': [('AGDEKNQRST', True, False), ('FWY', False, True)],
     'ph': [('FWY', False, True)]
 }
 
-predefined_resnums = [11, 12, 15, 16, 19, 24, 27, 30, 31, 35, 37, 39, 46, 52, 55, 56, 60, 65, 67, 69, 83, 85, 87, 98, 100, 102, 104, 106, 112, 115, 117, 119]
+predefined_resnums = [11, 12, 16, 24, 30, 31, 35, 37, 39, 46, 52, 55, 56, 60, 65, 67, 69, 83, 85, 87, 98, 100, 102, 104, 106, 112, 115, 117, 119]
 
 input_dict = {
     ('coo_0'):{
         'cg' : 'coo',
-        'lgd_sel' : ['C2', 'C1', 'O2', 'O1'],
+        'lgd_sel' : ['C8', 'C9', 'O3', 'O2'],
         'represent_name' : 'OD2',
         'correspond_resname' : 'ASP',
         'correspond_names' : ['CB', 'CG', 'OD1', 'OD2']
     },
     ('coo_1'):{
         'cg' : 'coo',
-        'lgd_sel' : ['C2', 'C1', 'O2', 'O1'],
+        'lgd_sel' : ['C8', 'C9', 'O3', 'O2'],
         'represent_name' : 'OD2',
         'correspond_resname' : 'ASP',
         'correspond_names' : ['CB', 'CG', 'OD2', 'OD1']
     },    
     ('coo_2'):{
         'cg' : 'coo',
-        'lgd_sel' : ['C2', 'C1', 'O2', 'O1'],
+        'lgd_sel' : ['C8', 'C9', 'O3', 'O2'],
         'represent_name' : 'OE2',
         'correspond_resname' : 'GLU',
         'correspond_names' : ['CG', 'CD', 'OE1', 'OE2']
     },
     ('coo_3'):{
         'cg' : 'coo',
-        'lgd_sel' : ['C2', 'C1', 'O2', 'O1'],
+        'lgd_sel' : ['C8', 'C9', 'O3', 'O2'],
         'represent_name' : 'OE2',
         'correspond_resname' : 'GLU',
         'correspond_names' : ['CG', 'CD', 'OE2', 'OE1']
     },
-    ('indole_0'):{
-        'cg' : 'indole',
-        'lgd_sel' : ['N1', 'C6', 'C5', 'C4'],
-        'represent_name' : 'NE1',
-        'correspond_resname' : 'TRP',
-        'correspond_names' : ['NE1', 'CE2', 'CD1', 'CG']
+    ('phenol_0'):{
+        'cg' : 'phenol',
+        'lgd_sel' : ['C2', 'C3', 'C4', 'O4'],
+        'represent_name' : 'OH',
+        'correspond_resname' : 'TYR',
+        'correspond_names' : ['CE1', 'CZ', 'CE2', 'OH']
     },
-    ('indole_1'):{
-        'cg' : 'indole',
-        'lgd_sel' : ['N1', 'C6', 'C5', 'C4'],
-        'represent_name' : 'NE1',
-        'correspond_resname' : 'TRP',
-        'correspond_names' : ['NE1', 'CD1', 'CE2', 'CD2']
-    },
-    ('hid_0'):{
-        'cg' : 'hid',
-        'lgd_sel' : ['N1', 'C6', 'C5', 'C4'],
-        'represent_name' : 'ND1',
-        'correspond_resname' : 'HIS',
-        'correspond_names' : ['ND1', 'CE1', 'CG', 'CD2']
-    },
-    ('hid_1'):{
-        'cg' : 'hid',
-        'lgd_sel' : ['N1', 'C6', 'C5', 'C4'],
-        'represent_name' : 'ND1',
-        'correspond_resname' : 'HIS',
-        'correspond_names' : ['ND1', 'CG', 'CE1', 'NE2']
-    },
-    ('hie_0'):{
-        'cg' : 'hie',
-        'lgd_sel' : ['N1', 'C6', 'C5', 'C4'],
-        'represent_name' : 'NE2',
-        'correspond_resname' : 'HIS',
-        'correspond_names' : ['NE2', 'CE1', 'CD2', 'CG']
-    },
-    ('hie_1'):{
-        'cg' : 'hie',
-        'lgd_sel' : ['N1', 'C6', 'C5', 'C4'],
-        'represent_name' : 'NE2',
-        'correspond_resname' : 'HIS',
-        'correspond_names' : ['NE2', 'CD2', 'CE1', 'ND1']
+    ('phenol_1'):{
+        'cg' : 'phenol',
+        'lgd_sel' : ['C2', 'C3', 'C4', 'O4'],
+        'represent_name' : 'OH',
+        'correspond_resname' : 'TYR',
+        'correspond_names' : ['CE2', 'CZ', 'CE1', 'OH']
     },
     ('bb_cco_0'):{
         'cg' : 'bb_cco',
-        'lgd_sel' : ['C2', 'C1', 'O2'],
+        'lgd_sel' : ['C8', 'C9', 'O2'],
         'represent_name' : 'O',
         'correspond_resname' : 'GLY',
         'correspond_names' : ['CA', 'C', 'O']
     },
     ('bb_cco_1'):{
         'cg' : 'bb_cco',
-        'lgd_sel' : ['C2', 'C1', 'O2'],
+        'lgd_sel' : ['C8', 'C9', 'O2'],
         'represent_name' : 'O',
         'correspond_resname' : 'ALA',
         'correspond_names' : ['CA', 'C', 'O']
     },
     ('bb_cco_2'):{
         'cg' : 'bb_cco',
-        'lgd_sel' : ['C2', 'C1', 'O2'],
+        'lgd_sel' : ['C8', 'C9', 'O2'],
         'represent_name' : 'O',
         'correspond_resname' : 'PRO',
         'correspond_names' : ['CA', 'C', 'O']
     },
     ('ph_0'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CG', 'CD1', 'CD2', 'CZ']
     },
     ('ph_1'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CG', 'CD2', 'CD1', 'CZ']
     },
     ('ph_2'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CD1', 'CG', 'CE1', 'CE2']
     },
     ('ph_3'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CD1', 'CE1', 'CG', 'CE2']
     },
     ('ph_4'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CE1', 'CD1', 'CZ', 'CD2']
     },
     ('ph_5'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CE1', 'CZ', 'CD1', 'CD2']
     },
     ('ph_6'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CZ', 'CE1', 'CE2', 'CG']
     },
     ('ph_7'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CZ', 'CE2', 'CE1', 'CG']
     },
     ('ph_8'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CE2', 'CZ', 'CD2', 'CD1']
     },
     ('ph_9'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CE2', 'CD2', 'CZ', 'CD1']
     },
     ('ph_10'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CD2', 'CE2', 'CG', 'CE1']
     },
     ('ph_11'):{
         'cg' : 'ph',
-        'lgd_sel' : ['C7', 'C8', 'C6', 'C10'],
+        'lgd_sel' : ['C1', 'C2', 'C6', 'C4'],
         'represent_name' : 'CG',
         'correspond_resname' : 'PHE',
         'correspond_names' : ['CD2', 'CG', 'CE2', 'CE1']
@@ -306,7 +275,7 @@ def run_all(file, workdir, path_to_database, ideal_ala_coords, lig_path):
 
     target_path = workdir + file
     #target_path = workdir + 'o2_1dmm_16-20-28_H-H-D_a_842.pdb'
-    outdir = workdir + '1dmm_i3pa_results/output_' + file + '_' + time_tag + '/'
+    outdir = workdir + '1dmm_tts_results/output_' + file + '_' + time_tag + '/'
     os.makedirs(outdir, exist_ok=True)
 
     target = search_lig_indep.prepare_rosetta_target(outdir, target_path, predefined_win_filters)
@@ -343,11 +312,7 @@ def run_all(file, workdir, path_to_database, ideal_ala_coords, lig_path):
         values = lig_vdm_dict[lig_id]
         exist_required_cg_contact = False
         for v in values:
-            if v[1][0] == 'indole' and v[1][-2] == True:
-                exist_required_cg_contact = True
-            if v[1][0] == 'hid' and v[1][-2] == True:
-                exist_required_cg_contact = True
-            if v[1][0] == 'hie' and v[1][-2] == True:
+            if v[1][0] == 'phenol' and v[1][-2] == True:
                 exist_required_cg_contact = True
 
         if not exist_required_cg_contact:
@@ -375,14 +340,14 @@ def run_all(file, workdir, path_to_database, ideal_ala_coords, lig_path):
         for v in values:
             vdm_ag = v[0]
             (cg, aa, resnum, cg_id, lig_id, _rmsd, vdm_id, score, v, contact_hb, contact_cc) = v[1]
-            if (cg, aa, resnum, vdm_id) in unique_vdms.keys():
+            if (cg, resnum, aa, vdm_id) in unique_vdms.keys():
                 continue
             else:
-                unique_vdms[(cg, aa, resnum, vdm_id)] = (vdm_ag, v)
+                unique_vdms[(cg, resnum, aa, vdm_id)] = (vdm_ag, v)
 
-    for (cg, aa, resnum, vdm_id) in unique_vdms.keys():
-        prefix = cg  + '_' + str(resnum) + '_' + aa  + '_' + str(vdm_id)                 
-        pr.writePDB(outdir_uni + prefix  + '.pdb.gz', unique_vdms[(cg, aa, resnum, vdm_id)][0])
+    for (cg, resnum, aa, vdm_id) in unique_vdms.keys():
+        prefix = cg  + '_' + str(resnum) +  '_' + aa + '_' + str(vdm_id)                 
+        pr.writePDB(outdir_uni + prefix  + '.pdb.gz', unique_vdms[(cg, resnum, aa, vdm_id)][0])
 
 
     ### Write summary file.
@@ -420,7 +385,7 @@ def main():
 
         workdir = '/wynton/home/degradolab/lonelu/GitHub_Design/Combs2_library/ntf2_fe_1dmm_rosetta_sel/'
 
-        lig_path = '/wynton/home/degradolab/lonelu/GitHub_Design/Combs2_library/ntf2_fe/i3pa_rdkit_fe.pdb'
+        lig_path = '/wynton/home/degradolab/lonelu/GitHub_Design/Combs2_library/ntf2_fe/tts_fe_rdkit.pdb'
     else:
         with open('/mnt/e/GitHub_Design/Metalprot/metalprot/constants/ideal_alanine_bb_only.pkl', 'rb') as f:
             ideal_alanine_bb_only = pickle.load(f)
@@ -429,8 +394,7 @@ def main():
         path_to_database='/mnt/e/DesignData/Combs/Combs2_database/'
         workdir = '/mnt/e/DesignData/ligands/LigandBB/_lig_fe/_ntf2_rosetta/output_sel/'
 
-        #lig_path = '/mnt/e/DesignData/ligands/LigandBB/_lig_fe/tts_fe_adj.pdb'
-        lig_path = '/mnt/e/DesignData/ligands/LigandBB/_lig_fe/i3pa_rdkit_fe.pdb'
+        lig_path = '/mnt/e/DesignData/ligands/LigandBB/_lig_fe/tts_fe_rdkit.pdb'
 
     print('on_wynton: ' + str(on_wynton))
  

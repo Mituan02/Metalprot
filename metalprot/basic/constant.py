e84e2a5135e42f01a14bf6347df0b67e0a406837
lonelu <lonelu@lonelus-MacBook-Pro.local>
2022-10-17 21:30:55 -0700

    xxx

--- metalprot/basic/constant.py
+++ metalprot/basic/constant.py
@@ -141,7 +141,7 @@ ideal_ala = pr.parsePDB(os.path.join(Path(__file__).parent.parent, 'constants/id
 try:
     with open(Path(__file__).parent.parent / 'constants/ideal_alanine_bb_only.pkl', 'rb') as f:
         ideal_alanine_bb_only = pickle.load(f)
+    ideal_ala_coords = np.array(ideal_alanine_bb_only[['c_x', 'c_y', 'c_z']])
 except:
-        ideal_alanine_bb_only = None
-        
-ideal_ala_coords = np.array(ideal_alanine_bb_only[['c_x', 'c_y', 'c_z']])
+    ideal_alanine_bb_only = None
+    ideal_ala_coords = None
